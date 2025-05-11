6c3af603be1cea59844402c8ba530a7692ccb470
Raymond Wong <raymondwong1701@gmail.com>
2025-05-10 17:03:34 -0700

    Remove old code

--- receiver/src/main.cpp
+++ receiver/src/main.cpp
@@ -62,7 +62,7 @@ void setup() {
 
   if (radio.init()) {
     tft.setTextSize(2);
-    tft.drawString("ESPNow Init success", width / 6, height / 2);
+    tft.drawString("Init success", width / 6, height / 2);
     Serial.println("ESPNow Init success");
   }
 
@@ -98,64 +98,7 @@ void onDataReady(uint32_t length) {
   renderJPEG(0, 0);
 }
 
-// void renderJPEG(int xpos, int ypos) {
-//   uint16_t *pImg;
-//   uint16_t mcu_w = JpegDec.MCUWidth;
-//   uint16_t mcu_h = JpegDec.MCUHeight;
-//   uint32_t max_x = JpegDec.width;
-//   uint32_t max_y = JpegDec.height;
-
-//   uint32_t min_w = mcu_w < max_x % mcu_w ? mcu_w : max_x % mcu_w;
-//   uint32_t min_h = mcu_h < max_y % mcu_h ? mcu_h : max_y % mcu_h;
-
-//   uint32_t win_w = mcu_w;
-//   uint32_t win_h = mcu_h;
-
-//   uint32_t drawTime = millis();
-
-//   while (JpegDec.read()) {
-//     pImg = JpegDec.pImage;
-//     int mcu_x = JpegDec.MCUx * mcu_w + xpos;
-//     int mcu_y = JpegDec.MCUy * mcu_h + ypos;
-
-//     if (mcu_x + mcu_w <= max_x) win_w = mcu_w;
-//     else win_w = min_w;
-//     if (mcu_y + mcu_h <= max_y) win_h = mcu_h;
-//     else win_h = min_h;
-
-//     // Allocate buffer for rotated MCU block
-//     uint16_t rotated[win_w * win_h];
-
-//     // Rotate 90° counter-clockwise into buffer
-//     for (uint16_t row = 0; row < win_h; row++) {
-//       for (uint16_t col = 0; col < win_w; col++) {
-//         uint16_t src_idx = row * win_w + col;
-//         uint16_t dst_idx = (win_w - 1 - col) * win_h + row;
-//         rotated[dst_idx] = pImg[src_idx];
-//       }
-//     }
-
-//     // Calculate rotated position on screen
-//     uint16_t x_rot = tft.height() - (mcu_y + win_h);  // Vertical position
-//     uint16_t y_rot = mcu_x;                           // Horizontal position
-
-//     if ((x_rot + win_w <= tft.height()) && (y_rot + win_h <= tft.width())) {
-//       tft.setAddrWindow(y_rot, x_rot, y_rot + win_h - 1, x_rot + win_w - 1);
-//       tft.pushColors(rotated, win_w * win_h);
-//     }
-//   }
-
-//   drawTime = millis() - drawTime;
-//   Serial.print("Total render time (fast CCW): ");
-//   Serial.print(drawTime);
-//   Serial.println(" ms");
-// }
-
-
-//working but 300 ms render time
-
 void renderJPEG(int xpos, int ypos) {
-
   // retrieve infomration about the image
   uint16_t *pImg;
   uint16_t mcu_w = JpegDec.MCUWidth;
@@ -230,78 +173,6 @@ void renderJPEG(int xpos, int ypos) {
   drawTime = millis() - drawTime; // Calculate the time it took
 
   // print the results to the serial port
-  Serial.print  ("Total render time was    : "); Serial.print(drawTime); Serial.println(" ms");
+  Serial.print("Total render time was    : "); Serial.print(drawTime); Serial.println(" ms");
   Serial.println("=====================================");
-
 }
-
-
-// void renderJPEG(int xpos, int ypos) {
-
-//   // retrieve infomration about the image
-//   uint16_t *pImg;
-//   uint16_t mcu_w = JpegDec.MCUWidth;
-//   uint16_t mcu_h = JpegDec.MCUHeight;
-//   uint32_t max_x = JpegDec.width;
-//   uint32_t max_y = JpegDec.height;
-
-//   // Jpeg images are draw as a set of image block (tiles) called Minimum Coding Units (MCUs)
-//   // Typically these MCUs are 16x16 pixel blocks
-//   // Determine the width and height of the right and bottom edge image blocks
-//   uint32_t min_w = mcu_w < max_x % mcu_w ? mcu_w : max_x % mcu_w;
-//   uint32_t min_h = mcu_h < max_y % mcu_h ? mcu_h : max_y % mcu_h;
-
-//   // save the current image block size
-//   uint32_t win_w = mcu_w;
-//   uint32_t win_h = mcu_h;
-
-//   // record the current time so we can measure how long it takes to draw an image
-//   uint32_t drawTime = millis();
-
-//   // save the coordinate of the right and bottom edges to assist image cropping
-//   // to the screen size
-//   max_x += xpos;
-//   max_y += ypos;
-
-//   // read each MCU block until there are no more
-//   while ( JpegDec.read()) {
-
-//     // save a pointer to the image block
-//     pImg = JpegDec.pImage;
-
-//     // calculate where the image block should be drawn on the screen
-//     int mcu_x = JpegDec.MCUx * mcu_w + xpos;
-//     int mcu_y = JpegDec.MCUy * mcu_h + ypos;
-
-//     // check if the image block size needs to be changed for the right and bottom edges
-//     if (mcu_x + mcu_w <= max_x) win_w = mcu_w;
-//     else win_w = min_w;
-//     if (mcu_y + mcu_h <= max_y) win_h = mcu_h;
-//     else win_h = min_h;
-
-//     // calculate how many pixels must be drawn
-//     uint32_t mcu_pixels = win_w * win_h;
-
-//     // draw image block if it will fit on the screen
-//     if ( (mcu_x + win_w) <= width && (mcu_y + win_h) <= height) {
-//       // open a window onto the screen to paint the pixels into
-//       //tft.setAddrWindow(mcu_x, mcu_y, mcu_x + win_w - 1, mcu_y + win_h - 1);
-//       tft.setAddrWindow(mcu_x, mcu_y, mcu_x + win_w - 1, mcu_y + win_h - 1);
-//       // push all the image block pixels to the screen
-//       while (mcu_pixels--) tft.pushColor(*pImg++); // Send to TFT 16 bits at a time
-//     }
-
-//     // stop drawing blocks if the bottom of the screen has been reached
-//     // the abort function will close the file
-//     else if ( ( mcu_y + win_h) >= height) JpegDec.abort();
-
-//   }
-
-//   // calculate how long it took to draw the image
-//   drawTime = millis() - drawTime; // Calculate the time it took
-
-//   // print the results to the serial port
-//   Serial.print  ("Total render time was    : "); Serial.print(drawTime); Serial.println(" ms");
-//   Serial.println("=====================================");
-
-// }
